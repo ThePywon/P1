6add20fb5d64b717309bd336d6db7ad316b1cd82
ThePywon <thepywon@hotmail.com>
2025-03-20 15:45:46 -0400

    Changed up tests a little

--- src/event/mod.rs
+++ src/event/mod.rs
@@ -3,3 +3,36 @@ mod event;
 
 pub use tick::Tick;
 pub use event::{Event, SimpleEvent, IntervalEvent};
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+  use std::cmp::Ordering;
+  use std::thread::sleep;
+  use std::time::Duration;
+
+  #[test]
+  fn compare_ticks() {
+    let mut a = Tick::new();
+    let mut b = a.clone();
+
+    assert!(a.cmp(&b) == Ordering::Equal);
+    assert!(b.cmp(&a) == Ordering::Equal);
+
+    sleep(Duration::from_secs(1));
+
+    a.touch();
+
+    assert!(a.cmp(&b) == Ordering::Greater);
+    assert!(b.cmp(&a) == Ordering::Less);
+
+    sleep(Duration::from_secs(1));
+
+    b.touch();
+
+    assert!(b.cmp(&a) == Ordering::Greater);
+    assert!(a.cmp(&b) == Ordering::Less);
+  }
+  
+  // Need more operators for ticks to test with
+}
